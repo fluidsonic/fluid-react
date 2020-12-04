6b85bf2f6703df73e9f4244711c2afe201d2795d
Marc Knaup <marc@knaup.io>
2020-12-04 18:31:06 +0000

    Added basic td/th attrs

--- modules/dom/sources/js/html/td.html.kt
+++ modules/dom/sources/js/html/td.html.kt
@@ -3,7 +3,12 @@ package io.fluidsonic.react
 import org.w3c.dom.*
 
 
-public external interface HTMLTableDataCellProps : HTMLProps
+public external interface HTMLTableDataCellProps : HTMLProps {
+
+	public var colSpan: Int?
+	public var headers: String?
+	public var rowSpan: Int?
+}
 
 
 @RDsl
