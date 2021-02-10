b42d94733d773c94c848a069a71e7105e4e2992a
Marc Knaup <marc@knaup.io>
2021-02-10 19:58:13 +0100

    🎉 0.9.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 	id("io.fluidsonic.gradle") version "1.1.21"
 }
 
-fluidLibrary(name = "react", version = "0.9.1") {
+fluidLibrary(name = "react", version = "0.9.2") {
 	allModules {
 		targets {
 			js {
