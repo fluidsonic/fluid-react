0ed3f2cf0f4ea8f541e38d032dd6dc472e3253da
Marc Knaup <marc@knaup.io>
2021-02-16 19:12:14 +0100

    Added textarea attrs 'cols', 'rows' & 'wrap'

--- modules/dom/sources-js/html/textarea.html.kt
+++ modules/dom/sources-js/html/textarea.html.kt
@@ -16,13 +16,16 @@ public external interface HTMLTextAreaProps : HTMLProps {
 	public var autoComplete: String?
 	public var autoCorrect: String?
 	public var autoFocus: Boolean?
+	public var cols: Int?
 	public var disabled: Boolean
 	public var placeholder: String?
 	public var readOnly: Boolean?
 	public var required: Boolean?
+	public var rows: Int?
 	public var spellCheck: String?
 	public var type: HTMLInputType?
 	public var value: String?
+	public var wrap: String?
 }
 
 
