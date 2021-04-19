493fe975ab2f1f4bd79b9145b6696e3075cd3499
Marc Knaup <marc@knaup.io>
2021-04-19 18:48:04 +0100

    🎉 0.10.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,21 +4,19 @@ plugins {
 	id("io.fluidsonic.gradle") version "1.1.22"
 }
 
-fluidLibrary(name = "react", version = "0.10.0") {
+fluidLibrary(name = "react", version = "0.10.1") {
 	allModules {
 		targets {
 			js {
 				custom {
-					compilations.all {
-						kotlinOptions.moduleKind = "commonjs"
-					}
+					useCommonJs()
 				}
 			}
 		}
 	}
 }
 
-fluidLibraryModule(description = "FIXME") {
+fluidLibraryModule(description = "Kotlin/JS wrapper for React") {
 	targets {
 		js {
 			dependencies {
